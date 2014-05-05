e9c9914171320d2ee866dc1f77644f0fbf1877d3
kodiers <kodiers@gmail.com>
2014-05-06 02:40:00 +0400

    Some changes in comments

--- dns/views.py
+++ dns/views.py
@@ -7,13 +7,11 @@ from django.core.context_processors import csrf
 # Create your views here.
 
 def reg_user(request):
-    # TODO: fix Exception Value: reg_user() takes exactly 1 argument (0 given)
     if request.method == 'POST':
         form = MyUserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            # Edite zone file
-
+            # Edit zone file
             return HttpResponseRedirect("/regsuccess/")
     else:
         form = MyUserCreationForm()
